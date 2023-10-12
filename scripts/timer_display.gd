7b6271e60e71f6f1e38d67b8b3d357ae8504ee1a
anti-matt-er <m.burrows@live.co.uk>
2023-10-12 22:54:44 +0100

    UI font tweaks

--- scripts/timer_display.gd
+++ scripts/timer_display.gd
@@ -1,12 +1,14 @@
 extends AutosizeLabel
 
 
-const NORMAL_COLOR := Color("9affcd")
+const NORMAL_COLOR := Color.WHITE
 const LOW_COLOR := Color("ff5143")
 
 
 func set_low(state: bool) -> void:
 	if state:
 		label_settings.font_color = LOW_COLOR
+		label_settings.outline_color = LOW_COLOR
 	else:
+		label_settings.font_color = NORMAL_COLOR
 		label_settings.font_color = NORMAL_COLOR
