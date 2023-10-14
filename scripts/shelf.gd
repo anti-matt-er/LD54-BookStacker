6f016e0e80cb20e3c1d063d7940f172dda23de17
anti-matt-er <m.burrows@live.co.uk>
2023-10-14 20:35:35 +0100

    Add bookstoppers

--- scripts/shelf.gd
+++ scripts/shelf.gd
@@ -10,14 +10,26 @@ const DISTANCE_FROM_WALL := 0.02
 const BOOK_PLACE_TRANSITION := 0.25
 const BOOK_PLACE_DELAY := 0.025
 
+@export var bookstopper_prefab: PackedScene
+
 @onready var remaining_length := LENGTH
 
 var books: Array[Book] = []
+var bookstopper_l: Node3D
+var bookstopper_r: Node3D
+var bookstopper_dimensions: Vector3
 
 signal stacked
 
 
+func _ready() -> void:
+	spawn_bookstoppers()
+
+
 func reset() -> void:
+	bookstopper_l.hide()
+	bookstopper_r.hide()
+	
 	remaining_length = LENGTH
 	for book in books:
 		book.pivot_helper.remove_child(book)
@@ -45,6 +57,8 @@ func stack_until_full() -> void:
 		book.animate_show(BOOK_PLACE_TRANSITION)
 		await get_tree().create_timer(BOOK_PLACE_DELAY).timeout
 	
+	position_bookstoppers()
+	
 	stacked.emit()
 
 
@@ -63,3 +77,31 @@ func stack_new_book() -> void:
 		-DEPTH / 2 + book.dimensions.z / 2 + DISTANCE_FROM_WALL 
 	)
 	remaining_length -= book.dimensions.x + MARGIN
+
+
+func spawn_bookstoppers() -> void:
+	bookstopper_l = bookstopper_prefab.instantiate()
+	bookstopper_r = bookstopper_prefab.instantiate()
+	
+	add_child(bookstopper_l)
+	add_child(bookstopper_r)
+	
+	bookstopper_dimensions = bookstopper_l.get_child(0, true).get_aabb().size
+	
+	bookstopper_l.global_position.y = global_position.y + (bookstopper_dimensions.y + THICKNESS) / 2
+	bookstopper_r.global_position.y = global_position.y + (bookstopper_dimensions.y + THICKNESS) / 2
+	
+	bookstopper_l.rotation.y = PI
+	
+	bookstopper_l.hide()
+	bookstopper_r.hide()
+
+
+func position_bookstoppers() -> void:
+	var distance = (LENGTH - remaining_length + bookstopper_dimensions.x) / 2
+	
+	bookstopper_l.global_position.x = global_position.x - distance
+	bookstopper_r.global_position.x = global_position.x + distance
+	
+	bookstopper_l.show()
+	bookstopper_r.show()
